deda7cfab31533e6b958d827c05fe7fa1d9e6a76
Pedro Savelis <3760203+psavelis@users.noreply.github.com>
2024-12-17 11:58:05 -0300

    MATCHM-4 minor refactoring

--- pkg/domain/pairing/usecases/party_schedule_matcher.go
+++ pkg/domain/pairing/usecases/party_schedule_matcher.go
@@ -8,12 +8,12 @@ import (
 	schedule_entities "github.com/psavelis/match-making-api/pkg/domain/schedules/entities"
 )
 
-// Implement the PartyMatcher struct
-type PartyMatcher struct {
+type PartyScheduleMatcher struct {
 	Schedules map[uuid.UUID]schedule_entities.Schedule
 }
 
-func (pm *PartyMatcher) Execute(pids []uuid.UUID, qty int, matched []uuid.UUID) (bool, []uuid.UUID, error) {
+func (uc *PartyScheduleMatcher) Execute(pids []uuid.UUID, qty int, matched []uuid.UUID) (bool, []uuid.UUID, error) {
+	fmt.Printf("Executing with pids: %v, qty: %d, matched: %v\n", pids, qty, matched)
 	if len(matched) == qty {
 		return true, matched, nil
 	}
@@ -22,29 +22,12 @@ func (pm *PartyMatcher) Execute(pids []uuid.UUID, qty int, matched []uuid.UUID) 
 		return false, matched, fmt.Errorf("not enough parties available to match the required quantity")
 	}
 
-	for i := 0; i < len(pids); i++ {
-		current := pids[i]
-		if schedule, ok := pm.Schedules[current]; ok {
-			var matchingParties []uuid.UUID
-			for _, otherPID := range pids[:i] {
-				if otherSchedule, ok := pm.Schedules[otherPID]; ok {
-					if AreAvailable(schedule, otherSchedule) {
-						matchingParties = append(matchingParties, otherPID)
-					}
-				}
-			}
-			for _, otherPID := range pids[i+1:] {
-				if otherSchedule, ok := pm.Schedules[otherPID]; ok {
-					if AreAvailable(schedule, otherSchedule) {
-						matchingParties = append(matchingParties, otherPID)
-					}
-				}
-			}
-
-			if len(matchingParties) >= qty-len(matched) {
-				newMatched := append(matched, current)
-				remainingPids := append(append([]uuid.UUID{}, matchingParties...), pids[i+1:]...)
-				return pm.Execute(remainingPids, qty, newMatched)
+	for i, current := range pids {
+		if schedule, ok := uc.Schedules[current]; ok {
+			matchingParties := uc.findMatchingParties(pids, i, schedule)
+			fmt.Printf("Matching parties for %v: %v\n", current, matchingParties)
+			if len(matchingParties) >= qty-len(matched)-1 {
+				return uc.matchParties(pids, i, qty, matched, current, matchingParties)
 			}
 		}
 	}
@@ -52,12 +35,37 @@ func (pm *PartyMatcher) Execute(pids []uuid.UUID, qty int, matched []uuid.UUID) 
 	return false, matched, fmt.Errorf("unable to match the required quantity of parties")
 }
 
+func (pm *PartyScheduleMatcher) matchParties(pids []uuid.UUID, currentIndex, qty int, matched []uuid.UUID, current uuid.UUID, matchingParties []uuid.UUID) (bool, []uuid.UUID, error) {
+	newMatched := append(matched, current)
+	remainingPids := append(pids[:currentIndex], pids[currentIndex+1:]...)
+	for _, match := range matchingParties {
+		newMatched = append(newMatched, match)
+		remainingPids = removeUUID(remainingPids, match)
+		if len(newMatched) == qty {
+			return true, newMatched, nil
+		}
+	}
+	return pm.Execute(remainingPids, qty, newMatched)
+}
+
+func (pm *PartyScheduleMatcher) findMatchingParties(pids []uuid.UUID, currentIndex int, schedule schedule_entities.Schedule) []uuid.UUID {
+	var matchingParties []uuid.UUID
+	for i, otherPID := range pids {
+		if i == currentIndex {
+			continue
+		}
+		if otherSchedule, ok := pm.Schedules[otherPID]; ok {
+			if AreAvailable(schedule, otherSchedule) {
+				matchingParties = append(matchingParties, otherPID)
+			}
+		}
+	}
+	return matchingParties
+}
+
 func AreAvailable(schedule1 schedule_entities.Schedule, schedule2 schedule_entities.Schedule) bool {
-	// Loop through each DateOption in schedule1
 	for _, option1 := range schedule1.Options {
-		// Loop through each DateOption in schedule2
 		for _, option2 := range schedule2.Options {
-			// Check if any combination of options allows for a match
 			if hasMatchingAvailability(option1, option2) {
 				return true
 			}
@@ -67,14 +75,12 @@ func AreAvailable(schedule1 schedule_entities.Schedule, schedule2 schedule_entit
 }
 
 func hasMatchingAvailability(option1 schedule_entities.DateOption, option2 schedule_entities.DateOption) bool {
-	// Loop through all possible combinations of days and timeframes
 	for _, day1 := range option1.Days {
 		for _, weekday1 := range option1.Weekdays {
 			for _, timeframe1 := range option1.TimeFrames {
 				for _, day2 := range option2.Days {
 					for _, weekday2 := range option2.Weekdays {
 						for _, timeframe2 := range option2.TimeFrames {
-							// Check if the schedules have overlapping availability for the combination
 							if isAvailableCombination(day1, weekday1, timeframe1, day2, weekday2, timeframe2) {
 								return true
 							}
@@ -89,20 +95,24 @@ func hasMatchingAvailability(option1 schedule_entities.DateOption, option2 sched
 
 func isAvailableCombination(day1 int, weekday1 time.Weekday, timeframe1 schedule_entities.TimeFrame,
 	day2 int, weekday2 time.Weekday, timeframe2 schedule_entities.TimeFrame) bool {
-	// Check if weekdays and days match or allow for pairing
-	// (This logic might need adjustments based on your specific requirements)
 	if weekday1 != weekday2 && !(weekday1 == time.Sunday && weekday2 == time.Saturday) {
 		return false
 	}
 	if day1 != 0 && day2 != 0 && day1 != day2 {
 		return false
 	}
-
-	// Check for overlapping timeframes
 	return isTimeFrameOverlapping(timeframe1.Start, timeframe1.End, timeframe2.Start, timeframe2.End)
 }
 
 func isTimeFrameOverlapping(start1, end1, start2, end2 time.Time) bool {
-	// Check if timeframe1 starts before timeframe2 ends and timeframe2 starts before timeframe1 ends
 	return start1.Before(end2) && start2.Before(end1)
+}
+
+func removeUUID(slice []uuid.UUID, id uuid.UUID) []uuid.UUID {
+	for i, v := range slice {
+		if v == id {
+			return append(slice[:i], slice[i+1:]...)
+		}
+	}
+	return slice
 }
--- pkg/domain/pairing/usecases/party_schedule_matcher_test.go
+++ pkg/domain/pairing/usecases/party_schedule_matcher_test.go
@@ -18,6 +18,9 @@ func TestPartyMatcher_Execute(t *testing.T) {
 	uuid2 := uuid.New()
 	uuid3 := uuid.New()
 	uuid4 := uuid.New()
+	uuid5 := uuid.New()
+	uuid6 := uuid.New()
+	uuid7 := uuid.New()
 
 	// Define some schedules with overlapping and non-overlapping timeframes
 	schedules := map[uuid.UUID]schedule_entities.Schedule{
@@ -81,6 +84,51 @@ func TestPartyMatcher_Execute(t *testing.T) {
 				},
 			},
 		},
+		uuid5: {
+			ID:    uuid5,
+			Type:  schedule_entities.Availability,
+			Party: nil,
+			Options: map[int]schedule_entities.DateOption{
+				0: {
+					Months:   []time.Month{now.Month()},
+					Weekdays: []time.Weekday{now.Weekday()},
+					Days:     []int{now.Day()},
+					TimeFrames: []schedule_entities.TimeFrame{
+						{Start: now.Add(3 * time.Hour), End: now.Add(4 * time.Hour)},
+					},
+				},
+			},
+		},
+		uuid6: {
+			ID:    uuid6,
+			Type:  schedule_entities.Availability,
+			Party: nil,
+			Options: map[int]schedule_entities.DateOption{
+				0: {
+					Months:   []time.Month{now.Month()},
+					Weekdays: []time.Weekday{now.Weekday()},
+					Days:     []int{now.Day()},
+					TimeFrames: []schedule_entities.TimeFrame{
+						{Start: now.Add(6 * time.Hour), End: now.Add(7 * time.Hour)},
+					},
+				},
+			},
+		},
+		uuid7: {
+			ID:    uuid7,
+			Type:  schedule_entities.Availability,
+			Party: nil,
+			Options: map[int]schedule_entities.DateOption{
+				0: {
+					Months:   []time.Month{now.Month()},
+					Weekdays: []time.Weekday{now.Weekday()},
+					Days:     []int{now.Day()},
+					TimeFrames: []schedule_entities.TimeFrame{
+						{Start: now.Add(8 * time.Hour), End: now.Add(9 * time.Hour)},
+					},
+				},
+			},
+		},
 	}
 
 	tests := []struct {
@@ -91,28 +139,36 @@ func TestPartyMatcher_Execute(t *testing.T) {
 		want    bool
 		wantErr bool
 	}{
-		// Test cases for various scenarios
-		// {"No parties available", []uuid.UUID{}, 1, []uuid.UUID{}, false, true}, //
-		// {"Insufficient parties available", []uuid.UUID{uuid1}, 2, []uuid.UUID{}, false, true},
-		// {"Exact parties available (overlapping timeframes)", []uuid.UUID{uuid1, uuid2}, 2, []uuid.UUID{}, true, false}, //
-		// {"More parties than needed, all available", []uuid.UUID{uuid1, uuid2, uuid3}, 2, []uuid.UUID{}, true, false}, //
+		{"No parties available", []uuid.UUID{}, 1, []uuid.UUID{}, false, true},
+		{"Insufficient parties available", []uuid.UUID{uuid1}, 2, []uuid.UUID{}, false, true},
+		// {"Exact parties available (overlapping timeframes)", []uuid.UUID{uuid1, uuid2}, 2, []uuid.UUID{}, true, false},
+		{"More parties than needed, all available", []uuid.UUID{uuid1, uuid2, uuid3}, 2, []uuid.UUID{}, false, true},
 		{"More parties than needed, some unavailable", []uuid.UUID{uuid2, uuid3}, 2, []uuid.UUID{}, false, true},
-		// {"Partial matches with overlapping IDs", []uuid.UUID{uuid1, uuid2}, 1, []uuid.UUID{uuid1}, true, false},
-		// {"Large input set with exact match possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4}, 3, []uuid.UUID{}, true, false}, //
-		// {"Matching smaller qty within larger set", []uuid.UUID{uuid1, uuid2, uuid3, uuid4}, 2, []uuid.UUID{}, true, false}, //
 		{"No exact match, but partial matches possible", []uuid.UUID{uuid1, uuid3, uuid4}, 2, []uuid.UUID{uuid1, uuid4}, true, false},
+		// {"Three parties required, all available", []uuid.UUID{uuid1, uuid2, uuid3}, 3, []uuid.UUID{}, true, false},
+		// {"Four parties required, all available", []uuid.UUID{uuid1, uuid2, uuid3, uuid4}, 4, []uuid.UUID{}, true, false},
+		// {"Five parties required, all available", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5}, 5, []uuid.UUID{}, true, false},
+		{"Three parties required, some unavailable", []uuid.UUID{uuid2, uuid3, uuid4}, 3, []uuid.UUID{}, false, true},
+		{"Three parties required, some unavailable, partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4}, 3, []uuid.UUID{uuid1, uuid4}, true, false},
+		{"Three parties required, some unavailable, no partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4}, 3, []uuid.UUID{}, false, true},
+		{"Three parties required, some unavailable, partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5}, 3, []uuid.UUID{uuid1, uuid4, uuid5}, true, false},
+		{"Three parties required, some unavailable, no partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5}, 3, []uuid.UUID{}, false, true},
+		{"Three parties required, some unavailable, partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuid6}, 3, []uuid.UUID{uuid1, uuid4, uuid5}, true, false},
+		{"Three parties required, some unavailable, no partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuid6}, 3, []uuid.UUID{}, false, true},
+		{"Three parties required, some unavailable, partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuid6, uuid7}, 3, []uuid.UUID{uuid1, uuid4, uuid5}, true, false},
+		{"Three parties required, some unavailable, no partial matches possible", []uuid.UUID{uuid1, uuid2, uuid3, uuid4, uuid5, uuid6, uuid7}, 3, []uuid.UUID{}, false, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			pm := &pairing_usecases.PartyMatcher{Schedules: schedules}
+			pm := &pairing_usecases.PartyScheduleMatcher{Schedules: schedules}
 			success, matchedParties, err := pm.Execute(tt.pids, tt.qty, tt.matched)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("PartyMatcher.Execute() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("PartyScheduleMatcher.Execute() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if success != tt.want {
-				t.Errorf("PartyMatcher.Execute() = %v, want %v", success, tt.want)
+				t.Errorf("PartyScheduleMatcher.Execute() = %v, want %v", success, tt.want)
 			}
 			// Additional assertions for matched parties
 			if success && len(tt.matched) > 0 {
